ee3aaa65a4fad4cfc506baf421fbe356378b199c
douglasbonifacio <douglasnanni@yahoo.com.br>
2023-10-31 20:30:58 -0300

    atualização 31-10

--- EtecBookUI/src/app/components/login/login.component.ts
+++ EtecBookUI/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid){
+      console.log(this.loginForm.value);
+      //Enviar os dados a API
+    } else {
+      //Exibir uma mensagem erro
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl){
+        control.markAsDirty({onlySelf:true})
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control)
+      }
+    })
+  }
 }
